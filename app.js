203427a05093eb406a78abf53b4ded8ba6f17154
aribakha <140427904+aribakha@users.noreply.github.com>
2023-10-28 19:58:57 +0500

    js assigment

---
+++ app.js
@@ -0,0 +1,221 @@
+                                //  CHAPTER # 5 ("MATH EXPRESSION")
+
+                                        // QUESTION # 1
+
+// var num1 = 3
+// var num2 = 5
+// var sum = num1 + num2
+// document.write("sum of 3 and 5 is "+ sum)
+
+
+                                             // QUESTION # 2
+
+                                          // PART 1 (substraction)
+// var num1 = 3
+// var num2 = 5
+// document.write( num1 - num2)
+
+                                          // PART 2 (multiplication)
+
+// var num1 = 3
+// var num2 = 5
+// document.write( num1 * num2 )
+
+                                          // PART 3 (division)
+
+// var num1 = 3
+// var num2 = 5
+// document.write( num1 / num2 )
+
+                                          // PART 4 (modulus)
+
+// var num1 = 3
+// var num2 = 5
+// document.write( num1 % num2 )
+
+
+                                             // QUESTION # 3
+
+// var message1 = "value of variable declaration is undefined"
+// document.write( message1 +"<br>" )
+// var num1 = 5
+// var message2 = "initial value : 5"
+// document.write( message2 +"<br>")
+// document.write("Value after increment is : " + ++num1 +"<br>" )
+// var num2 = 6
+// var num3 = 7
+// var sumNum = num2 + num3
+// document.write( "Value after addition is : "+ sumNum  +"<br>")
+// document.write("Value after decrement is : " + --sumNum +"<br>" )
+// var num4 = 4
+// var num5 = 2
+// document.write("The remainder is : " + num4 % num5) 
+
+                                             // QUESTION # 4
+
+// var ticketPrice = 600
+// var bying = 5
+// document.write( "Total cost to buy 5 tickets to a movie is " + ticketPrice * bying +" PKR" )                                            
+
+                                             // QUESTION # 5
+// document.write("Table of 4" +"<br>")
+// document.write( 4 +" x 1 = "+ 4*1 +"<br>")
+// document.write( 4 +" x 2 = "+ 4*2 +"<br>")
+// document.write( 4 +" x 3 = "+ 4*3 +"<br>")
+// document.write( 4 +" x 4 = "+ 4*4 +"<br>")
+// document.write( 4 +" x 5 = "+ 4*5 +"<br>")
+// document.write( 4 +" x 6 = "+ 4*6 +"<br>")
+// document.write( 4 +" x 7 = "+ 4*7 +"<br>")
+// document.write( 4 +" x 8 = "+ 4*8 +"<br>")
+// document.write( 4 +" x 9 = "+ 4*9 +"<br>")
+// document.write( 4 +" x 10 = "+ 4*10 +"<br>")
+
+                                             // QUESTION # 6
+
+var cF = prompt("Enter c or f");
+if(cF == "c"){
+        var value = prompt("Enter the value" +"cF");
+}
+else{
+        var value = prompt("Enter the value" +"cF"); 
+}
+if ( cF == "c")
+{
+        console.log("c"+ value)
+}
+else{
+        console.log( "f"+ value)
+}
+
+                                             // QUESTION # 7
+
+// var price1 = 650
+// var quantity1 = 3
+// var  price2 = 100
+// var quantity2 = 7
+// var charges = 100
+// document.write("<h1>Shopping Cart</h1>")
+// document.write("Price of item 1 is " + price1 +"</br>")
+// document.write("quantity of item 1 is " +quantity1 +"</br>")
+// document.write("Price of item 2 is " + price2 +"</br>")
+// document.write("Quantity of item 2 is " + quantity2 +"</br>")
+// document.write("Total cost of your order is ")
+
+
+
+
+
+
+
+
+                                             // QUESTION # 8
+// var totalMark = 980
+// var markObtained = 804
+// document.write("<h1>Marks Sheet</h1>")
+// document.write("Total marks : "+ totalMark +"<br>")
+// document.write("Marks obtained : "+ markObtained +"<br>" )
+// document.write("Percentage : "+ markObtained * 100 /totalMark  +" % ")
+
+                                             // QUESTION # 9
+
+// var totalDollar = 10                                           
+// var usDollar =104.80
+// var dollarAmount = totalDollar * usDollar
+// var totalRiyal = 25
+// var saudiRiyal = 28
+// var saudiAmount = totalRiyal * saudiRiyal
+// var amountPkr = dollarAmount + saudiAmount
+// document.write("<h1>Total Currency In PKR </h1>")
+// document.write( " Total Currency in PKR : " + amountPkr )
+
+
+                                             // QUESTION # 10
+
+// var initialNum = 2
+// document.write(initialNum + 5 * 10 / 2)
+
+                                             // QUESTION # 11
+
+// var currentYear = 2023
+// var birthYear = 1996
+// var ageNow = currentYear - birthYear
+// document.write("<h1>Age Calculator</h1>")  
+// document.write("Current year : "+ currentYear +"<br>")    
+// document.write("Birth year : "+ birthYear +"<br>")                                           
+// document.write("Your age is : "+ ageNow)
+
+                                             // QUESTION # 12
+                                            
+// var radius = 20
+// var pie = 3.142
+// var formulaCircumference = 2* 3.142*20
+// var formulaArea = 3.142*20*20
+// document.write("Radius of a circle is : " + radius +"</br>")
+// document.write("The circumference is : "+ formulaCircumference +"</br>")
+// document.write("The area is : "+formulaArea )
+
+                                             // QUESTION # 13
+// var favouriteSnack = kurkury
+// document.write( favouriteSnack )
+// var currentAge = 27
+// var maximumAge = 60
+// var perDay = 5
+// document.write( "Favourite snack :  "+ favouriteSnack )
+// document.write( "Current age : " + currentAge )
+// document.write( "Estimated Maximum Age: " + maximumAge )
+// document.write( "Amount of snacks per day : " + perDay)
+
+                                // CHAPTER # 6 " MATH EXPRESSION"
+
+                                      // QUESTION # 1
+var aNum = 10
+document.write("result : + <br>The value of a is " + aNum)
+
+
+
+                                      // QUESTION # 3
+// var subject = English
+// var subject = Math
+// var subject = Urdu
+
+// document.write("<h1>Subject  Total Marks  Obtained Marks  Percentage</h1>")
+// document.write( $1subject +$1total +$1obtained   )
+
+// var $1subject = English
+// var $2subject = Math
+// var $3subject = Urdu
+// var $1total = 100
+// var $2total = 100
+// var $3total = 100
+// var $1obtained = 54
+// var $2obtained = 54
+// var $3obtained = 48
+// document.write($1subject + $2subject  +$3subject)
+
+
+                                        // QUESTION # 3
+
+// let userInput = prompt("Enter a number ");
+// let numberToDisplay = parseInt (userInput) || 5;
+// for ( let i = 1; i <= 10; i++ ){
+//         document.write(numberToDisplay + " X "+ i + "=" +(numberToDisplay * i) +"</br>")
+// }
+        
+// var subject1 = prompt("Enter the first subject")
+// var subject2 = prompt("Enter the first subject")
+// var subject3 = prompt("Enter the first subject")
+
+//                        //  total marks for each subject is 100
+
+// var totalMarks = 100 
+
+//                      // Get obtained marks for each subject  from the user
+
+// var obtainedMarks1 = prompt("Enter a number")
+// var obtainedMarks2 = prompt("Enter a number")
+// var obtainedMarks3 = prompt("Enter a number")
+
+
+//                      // Get obtained marks for each subject  from the user
+// var totalObatined = "obtainedMarks1" + "obtainedMarks2" + "obtainedMarks3"
+// document.write(subject1,subject2,subject3)
